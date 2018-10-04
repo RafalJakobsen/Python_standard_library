89cbbb11f9790d6647e586cae282959b1dc2af29
Rafal Jakobsen <rafal.jakobsen@protonmail.com>
2018-10-04 22:50:50 +0200

    min and max

---
+++ Built-In/min and max.py
@@ -0,0 +1,14 @@
+# Minimum and Maximum
+
+playerOneScore = 10
+playerTwoScore = 4
+print(min(playerOneScore, playerTwoScore))
+print(min(0, 12, -12))
+
+print(min("Kathryn", "Katie"))
+print(min("Angela", "Bob"))
+
+print(max(playerOneScore, playerTwoScore))
+playerThreeScore = 14
+print(max(playerTwoScore, playerThreeScore, playerOneScore))
+print(max("Katrhin", "Katie"))
