eb0bd4f2fce696ffebbc6eab44be06b7afaf6886
Rafal Jakobsen <rafal.jakobsen@protonmail.com>
2018-10-04 23:44:37 +0200

    Rounding, absolute value, and exponents

---
+++ Built-In/rounding, absolute value, and exponents.py
@@ -0,0 +1,28 @@
+# Python Rounding, Absolute Value, and Exponents
+
+# round ()
+myGPA = 3.6
+print(round(myGPA))
+amountOfSalt = 1.4
+print(round(amountOfSalt))
+
+apple = -1.2
+print(round(apple))
+google = -1.6
+print(round(google))
+
+# abs ()
+distanceAway = -13
+print(abs(distanceAway))
+lenghtOfRootInGround = -2.5
+print(abs(lenghtOfRootInGround))
+
+#pow ()
+chanceOfTails = 0.5
+inARowTails = 3
+print(pow(chanceOfTails, inARowTails))
+
+chanceOfOne = .167
+inARowOne = 2
+print(pow(chanceOfOne, inARowOne))
+
