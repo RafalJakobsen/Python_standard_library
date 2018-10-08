a90b297e537a272676ff4204650f291cdef13ce8
Rafal Jakobsen <rafal.jakobsen@protonmail.com>
2018-10-08 23:20:34 +0200

    getting the current time

---
+++ Data & Time, HTML Parsing and HTTP/getting the current time with python.py
@@ -0,0 +1,11 @@
+# Datetime Module Part 1
+from datetime import datetime
+
+now = datetime.now()
+print(now.date())
+print(now.year)
+print(now.month)
+print(now.hour)
+print(now.minute)
+print(now.second)
+print(now.time())
