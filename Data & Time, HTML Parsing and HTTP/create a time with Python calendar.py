a99ba7b98000a2c2aea4da07609ffe13dc730ec6
Rafal Jakobsen <rafal.jakobsen@protonmail.com>
2018-10-08 23:42:55 +0200

    create a timer with python time

---
+++ Data & Time, HTML Parsing and HTTP/create a time with Python calendar.py
@@ -0,0 +1,13 @@
+# Create a Timer with the Time module
+
+import time
+
+run = input("Start? >")
+seconds = 0
+
+if run == "yes":
+    while seconds != 10:
+        print(">", seconds)
+        time.sleep(1)
+        seconds += 1
+
