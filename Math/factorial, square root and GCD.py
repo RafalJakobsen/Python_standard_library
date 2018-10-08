f1522730ac9e7e7e8765573e169bc8cc491a8f3a
Rafal Jakobsen <rafal.jakobsen@protonmail.com>
2018-10-08 20:57:36 +0200

    Factorial, square root, and GCD

---
+++ Math/factorial, square root and GCD.py
@@ -0,0 +1,18 @@
+# Math Module Part 2
+import math
+
+# Factorial & Square Root
+print(math.factorial(3))
+print(math.sqrt(64))
+
+# Greatest Common Denominator GCD
+print(math.gcd(52, 8))
+print(math.gcd(8, 52))
+
+print(8/52)
+print(2/13)
+
+# Degrees and Radians
+print(math.radians(360))
+print(math.pi * 2)
+print(math.degrees(math.pi * 2))
